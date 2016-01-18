569fadda81b02521eda1ee1137bc500909cebbac
Breyten Ernsting <bje@dds.nl>
2016-01-18 13:54:33 +0100

    Disable sidebar everywhere.

--- lib/setup.php
+++ lib/setup.php
@@ -80,15 +80,18 @@ add_action('widgets_init', __NAMESPACE__ . '\\widgets_init');
 function display_sidebar() {
   static $display;
 
-  isset($display) || $display = !in_array(true, [
-    // The sidebar will NOT be displayed if ANY of the following return true.
-    // @link https://codex.wordpress.org/Conditional_Tags
-    is_404(),
-    is_front_page(),
-    is_page_template('template-custom.php'),
-  ]);
-
-  return apply_filters('sage/display_sidebar', $display);
+  // isset($display) || $display = !in_array(true, [
+  //   // The sidebar will NOT be displayed if ANY of the following return true.
+  //   // @link https://codex.wordpress.org/Conditional_Tags
+  //   is_404(),
+  //   is_front_page(),
+  //   is_page_template('template-custom.php'),
+  // ]);
+  //
+  // return apply_filters('sage/display_sidebar', $display);
+    $display = false;
+    apply_filters('sage/display_sidebar', $display);
+    return $display;
 }
 
 /**
