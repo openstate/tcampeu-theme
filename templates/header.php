1f21cf37bc34537a63f2194dce397d3ea417d5ca
Breyten Ernsting <bje@dds.nl>
2016-01-19 12:19:36 +0100

    Various layout changes.

--- templates/header.php
+++ templates/header.php
@@ -1,6 +1,12 @@
 <header class="banner">
   <div class="container">
-    <a class="brand" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
+    <div class="row header-bg">
+      <a href="<?= esc_url(home_url('/')); ?>">
+      <img class="img-responsive display-inline" src="<?= get_template_directory_uri() . '/dist/images/logo-tceu.png'; ?>" alt="TransparencyCamp Europe logo"></img>
+      </a>
+      <h1 class="tceu-header"><a class="brand" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a></h1>
+    </div>
+    
     <nav class="nav-primary">
       <?php
       if (has_nav_menu('primary_navigation')) :
