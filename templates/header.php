e7a54af9b62ade986454bbce959b7a6426fb2311
Sicco van Sas <sicco@openstate.eu>
2016-01-22 13:50:49 +0100

    Made hamburger menu fixed in the upper right corner

--- templates/header.php
+++ templates/header.php
@@ -7,13 +7,32 @@
         </a>
         <h1 class="tceu-header"><a class="brand" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a></h1>
       </div>
-    
-      <div class="col-lg-5 pull-right">
+      <nav class="hidden-lg nav-primary navbar-default navbar-fixed-top">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+        </div>
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+          <br>
+          <?php
+          if (has_nav_menu('primary_navigation')) :
+            wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']);
+          endif;
+          ?>
+          <br>
+        </div><!-- /.navbar-collapse -->
+      </nav>
+      <div class="visible-lg-inline col-lg-5 pull-right">
         <br>
         <div class="row">
           <div class="col-xs-12 bg-white">
             <nav class="nav-primary navbar-default bg-white">
-              <div class="navbar-header navbar-fixed-top">
+              <div class="navbar-header">
                 <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
                   <span class="sr-only">Toggle navigation</span>
                   <span class="icon-bar"></span>
@@ -23,11 +42,13 @@
               </div>
               <!-- Collect the nav links, forms, and other content for toggling -->
               <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <br>
                 <?php
                 if (has_nav_menu('primary_navigation')) :
                   wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']);
                 endif;
                 ?>
+                <br>
               </div><!-- /.navbar-collapse -->
             </nav>
           </div>
