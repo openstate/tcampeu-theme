9bc4db5a7ef340c328519a1653b2c2e4d5a83268
Sicco van Sas <sicco@openstate.eu>
2016-01-25 15:13:11 +0100

    Change the way the footer layout looks on xs

--- templates/footer.php
+++ templates/footer.php
@@ -1,7 +1,7 @@
 <footer class="content-info">
   <div class="container bg-grey-light">
     <br>
-    <div>
+    <div class="hidden-xs">
       <div class="pull-left">
         <a class="left-footer-padding" href="http://openstate.eu/" target="_blank"><img class="left-footer-image" src="<?= get_template_directory_uri() . '/dist/images/logo-open-state-grey.svg'; ?>" alt="Open State Foundation logo"></a>
         <a class="left-footer-padding" href="https://www.eu2016.nl/" target="_blank"><img class="left-footer-image" src="<?= get_template_directory_uri() . '/dist/images/logo-eunl2016-grey.svg'; ?>"alt="The Netherlands EU Presidency 2016 logo"></a>
@@ -15,10 +15,21 @@
           <li><a class="text-grey hashtag-font-size" href="https://twitter.com/search?f=tweets&q=tcampeu" target="_blank">#TCampEU</a></li>
         </ul>
       </div>
-    <div>
-    <br>
-    <br>
-    <br>
-    <br>
+      <br>
+      <br>
+      <br>
+      <br>
+    </div>
+    <div class="visible-xs-block text-center">
+        <a class="left-footer-padding" href="http://openstate.eu/" target="_blank"><img class="left-footer-image" src="<?= get_template_directory_uri() . '/dist/images/logo-open-state-grey.svg'; ?>" alt="Open State Foundation logo"></a>
+        <a href="https://www.eu2016.nl/" target="_blank"><img class="left-footer-image" src="<?= get_template_directory_uri() . '/dist/images/logo-eunl2016-grey.svg'; ?>"alt="The Netherlands EU Presidency 2016 logo"></a>
+        <ul class="list-inline text-grey right-footer-list-padding">
+          <li><a class="right-footer-padding" href="https://www.facebook.com/events/1689822561284709/" target="_blank"><i class="fa fa-facebook fa-2x text-grey"></i></a></li>
+          <li><a class="right-footer-padding" href="https://twitter.com/openstateeu" target="_blank"><i class="fa fa-twitter fa-2x text-grey"></i></a></li>
+          <li><a class="right-footer-padding" href="https://github.com/openstate" target="_blank"><i class="fa fa-github fa-2x text-grey"></i></a></li>
+          <li><a class="right-footer-padding" href="mailto:tcampeu@openstate.eu"><i class="fa fa-envelope-o fa-2x text-grey"></i></a></li>
+          <li><a class="text-grey hashtag-font-size" href="https://twitter.com/search?f=tweets&q=tcampeu" target="_blank">#TCampEU</a></li>
+        </ul>
+    </div>
   </div>
 </footer>
