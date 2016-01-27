b086ffa91dc16de9a85715ac9dfb93ca796a1bdf
Sicco van Sas <sicco@openstate.eu>
2016-01-27 12:09:06 +0100

    Fix typo in text again

--- templates/frontpage-content.php
+++ templates/frontpage-content.php
@@ -34,7 +34,7 @@
     <div class="col-md-4 col-md-offset-2 text-center">
       <h1 class="text-blue big-h1">App Competition</h1>
       <p class="frontpage-content-text">
-        You can join the online App Competition, either by building an application that improves transparency and opens the black box of EU decision-making, or submitting apps that use European open data (e.g. on mobility, public health, education or the environment) to improve the every day lives of Europeans.
+        You can join the online App Competition, either by building an application that improves transparency and opens the black box of EU decision-making, or submitting apps that use European open data (e.g. on mobility, public health, education or the environment) to improve the everyday lives of Europeans.
       </p>
       <div class="row text-blue frontpage-content-icons">
         <div class="col-xs-12 text-center">
