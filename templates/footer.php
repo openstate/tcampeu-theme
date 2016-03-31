d3d0f67141b1f36ea7bd978b92cac4fc4ac3f8c4
Sicco van Sas <sicco@openstate.eu>
2016-03-31 16:15:30 +0200

    Change Twitter link from @OpenStateEU to @TCampEU

--- templates/footer.php
+++ templates/footer.php
@@ -9,7 +9,7 @@
       <div class="pull-right">
         <ul class="list-inline text-grey right-footer-list-padding">
           <li><a class="right-footer-padding" href="https://www.facebook.com/events/1689822561284709/" target="_blank"><i class="fa fa-facebook fa-2x text-grey"></i></a></li>
-          <li><a class="right-footer-padding" href="https://twitter.com/openstateeu" target="_blank"><i class="fa fa-twitter fa-2x text-grey"></i></a></li>
+          <li><a class="right-footer-padding" href="https://twitter.com/TCampEU" target="_blank"><i class="fa fa-twitter fa-2x text-grey"></i></a></li>
           <li><a class="right-footer-padding" href="https://github.com/openstate" target="_blank"><i class="fa fa-github fa-2x text-grey"></i></a></li>
           <li><a class="right-footer-padding" href="mailto:tcampeu@openstate.eu"><i class="fa fa-envelope-o fa-2x text-grey"></i></a></li>
           <li><a class="text-grey hashtag-font-size" href="https://twitter.com/search?f=tweets&q=tcampeu" target="_blank">#TCampEU</a></li>
