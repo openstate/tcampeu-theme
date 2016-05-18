c0d594dddc981a3719176afe8d2aa6306726fe16
Sicco van Sas <sicco@openstate.eu>
2016-05-18 20:54:14 +0200

    Add a separate Schedule page for viewing on LCD monitors on the event veneu

---
+++ base-schedule.php
@@ -0,0 +1,21 @@
+<?php
+
+use Roots\Sage\Setup;
+use Roots\Sage\Wrapper;
+
+?>
+
+<!doctype html>
+<html <?php language_attributes(); ?>>
+  <?php get_template_part('templates/head'); ?>
+  <body <?php body_class(); ?>>
+    <!--[if IE]>
+      <div class="alert alert-warning">
+        <?php _e('You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.', 'sage'); ?>
+      </div>
+    <![endif]-->
+    <main class="main">
+      <?php include Wrapper\template_path(); ?>
+    </main><!-- /.main -->
+  </body>
+</html>
---
+++ schedule.php
@@ -0,0 +1,9 @@
+<?php
+/*
+ * Template Name: Schedule
+ * */
+?>
+
+<?php while (have_posts()) : the_post(); ?>
+  <?php the_content(); ?>
+<?php endwhile; ?>
